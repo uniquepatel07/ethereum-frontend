70b5299d047816fcb1b755eac7a1d1e7ddbc6e50
uniquepatel01 <uniquepatel01@gmail.com>
2024-02-11 14:21:45 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes instead of Switch
+import HomePage from './HomePage';
+import TransactionPage from './TransactionPage';
+
+
+
+
+function App() {
+
+
+  return (
+    <Router>
+
+      <Routes> {/* Use Routes instead of Switch */}
+        <Route exact path="/" element={<HomePage />} /> {/* Use element prop instead of component */}
+        <Route exact path="/" element={<HomePage />} /> {/* Use element prop instead of component */}
+        <Route path="/transactions" element={<TransactionPage />} /> {/* Use element prop instead of component */}
+      </Routes> {/* Use Routes instead of Switch */}
+    </Router>
+  );
+}
+
+export default App;
